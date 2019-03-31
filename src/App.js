e97b373473ad4b86903b35571919757ca69bbc06
Bryan Besnyi <besnyib@gmail.com>
2019-03-31 15:47:32 -0700

    feat: console log test for book data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import ListBooks from "./ListBooks";
 import Search from "./Search";
 import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {};
+  state = { books: [] };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
 
   render() {
+    console.log(this.state.books);
     return (
       <div className="app">
         <Route exact path="/" render={() => <ListBooks />} />
