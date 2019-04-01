b02c2f7ca0becc1ba3de3b6920d156f171c4f528
Bryan Besnyi <besnyib@gmail.com>
2019-03-31 20:37:31 -0700

    feat: turn bookshelf change dropdown to component

---
+++ src/BookshelfChanger.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+func BookshelfChanger() {
+    render (
+        <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+    )
+}
+
+export default BookshelfChanger
