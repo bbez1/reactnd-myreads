7cd0cc7e0ce12ed86395e4e219699c2b839db1e4
Bryan Besnyi <besnyib@gmail.com>
2019-03-31 21:37:51 -0700

    Revert "feat: console log test for book data"
    This reverts commit e97b373473ad4b86903b35571919757ca69bbc06.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import * as BooksAPI from "./BooksAPI";
+// import * as BooksAPI from './BooksAPI'
 import "./App.css";
 import ListBooks from "./ListBooks";
 import Search from "./Search";
 import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = { books: [] };
-
-  componentDidMount() {
-    BooksAPI.getAll().then(books => this.setState({ books }));
-  }
+  state = {};
 
   render() {
-    console.log(this.state.books);
     return (
       <div className="app">
         <Route exact path="/" render={() => <ListBooks />} />
